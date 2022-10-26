10d0a6bd74e3d211046502cad347ca1027898f1f
Meinert, Ian D <ianmeinert@gmail.com>
2022-10-26 07:41:26 -0500

    Section 9 and 10 exercises

---
+++ section_nine/error_handling.py
@@ -0,0 +1,17 @@
+# Error handling
+from multiprocessing.sharedctypes import Value
+
+
+while True:
+    try:
+        age = int(input('what is your age?'))
+        10/age
+    except ValueError:
+        print('please enter a number')
+    except ZeroDivisionError:
+        print('please enter age higher than 0')
+    else:
+        print('thank you!')
+        break
+    finally:
+        print('ok, i am finally done')
